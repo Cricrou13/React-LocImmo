ba611574b4ca4825c11611695e423c4918b0ac6b
Christophe <delclos.c13@gmail.com>
2026-02-13 18:59:26 +0100

    Correction erreurs carrousel

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -4,9 +4,7 @@ import "./Slideshow.scss";
 const Slideshow = (props) => {
 // 'pictures' est le tableau d'URLs qui vient du JSON
 
-const stateArray = useState [0];
-const currentIndex = stateArray[0];
-const SetCurrentIndex = stateArray[1];
+ const [currentIndex, SetCurrentIndex] = useState(0);
 
 /* Si je suis à la dernière image, je reviens à la première (0). Sinon, je fais +1. */
 const nextSlide = () => {
