3feb6dc180499bc37d03e6a4f5f98798f6ce62cf
Christophe <delclos.c13@gmail.com>
2026-02-17 05:29:39 +0100

    Déploiement Git

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/React-LocImmo/",
-});
+  // Si on est en train de déployer (build), on utilise le nom du repo
+  // Sinon (en mode dev sur l'ordi), on utilise la racine "/"
+  base: command === "build" ? "/React-LocImmo/" : "/",
+}));
